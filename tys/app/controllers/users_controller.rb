147f32c5c12b62c1586b7626e970156bd0b1b5b0
AnSlvt <andrea.slvt94@gmail.com>
2016-09-29 10:58:31 +0200

    Refined login process

--- tys/app/controllers/users_controller.rb
+++ tys/app/controllers/users_controller.rb
@@ -3,20 +3,34 @@ require 'net/http'
 class UsersController < ApplicationController
 
   @@state = ""
-  @access_token = ""
-  @@client_id = 'd64b686c8f0aa243775d'
-  @@client_secret = '54e803105611548081f92ea6a90d59ec31358b98'
 
   def login
     if @@state.to_s == ""
       get_request_code
     else
       code = params[:code]
-      get_access_token(code)
+      access_token = get_access_token(code)
       @@state = ""
-      client = Octokit::Client.new(access_token: @access_token)
-      @user = client.user
+      client = Octokit::Client.new(access_token: access_token)
+      user = client.user
+      session[:user_id] = user.login.to_s
+      flash[:notice] = "#{user.login} succesfully logged in!"
+      @user = User.find_by(name: user.login.to_s)
+      if (!@user)
+        begin
+          @user = User.create!(name: user.login.to_s,
+                               email: client.emails[0][:email].to_s)
+        rescue RecordInvalid
+          render file: 'public/500.html' and return
+        end
+      end
     end
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to '/'
+    flash[:notice] = "Logged out!"
   end
 
   private
@@ -24,24 +38,25 @@ class UsersController < ApplicationController
   def get_request_code
     uri = URI("https://github.com/login/oauth/authorize")
     @@state = SecureRandom.base64
-    params = { client_id: @@client_id.to_s,
+    params = { client_id: ENV['client_id'],
                scope: "user,repo",
                state: @@state.to_s }
     uri.query = URI.encode_www_form(params)
     redirect_to uri.to_s, method: :get
-    logger.info "#{uri}"
   end
 
   def get_access_token(code)
     uri = URI('https://github.com/login/oauth/access_token')
-    prm = { client_id: @@client_id,
-            client_secret: @@client_secret,
+    prm = { client_id: ENV['client_id'],
+            client_secret: ENV['client_secret'],
             code: code,
             state: @@state }
     uri.query = URI.encode_www_form(prm)
     res = Net::HTTP.post_form(uri, "q" => uri.query)
+    reg_try = res.body.scan(/\=([a-z0-9]+)&\1 in/)
+    logger.info "REGEX #{reg_try}"
     temp = res.body.scan(/\=[a-z0-9]*&/)[0]
-    @access_token = temp.slice(1, temp.size - 2)
+    temp.slice(1, temp.size - 2)
   end
 
 end
--- tys/config/routes.rb
+++ tys/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "homepage#index"
   #resources :users
   get 'users/login' => 'users#login'
+  get 'users/logout' => 'users#logout'
 end
