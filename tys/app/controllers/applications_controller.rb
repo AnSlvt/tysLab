141261cf6c0fdc24ffc551e3804fef289656fbdf
LeonardoPetrucci <leonardo.petrucci94@gmail.com>
2016-10-03 11:33:13 +0200

    first iteration again

--- tys/app/controllers/applications_controller.rb
+++ tys/app/controllers/applications_controller.rb
@@ -4,9 +4,9 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
   end
 
-  #creates a new application, which author is che current user (redirection to a form that execute a POST to invoke the create method)
+  #creates a new application, which author is the current user (redirection to a form that execute a POST to invoke the create method)
   def new
-    @application = @current_user.applications.build #does it render the creation form?
+    #@application = @current_user.applications.build #does it render the creation form?
   end
   #alternative
   #def new
@@ -15,13 +15,22 @@ class ApplicationsController < ApplicationController
 
   #after "new" action, the real creation
   def create
-    @current_user.applications << @current_user.applications.build(params[:application]) #not sure if it's possible
+    #@current_user.applications << @current_user.applications.build(params[:application]) #not sure if it's possible
+    #@application.save!
+    @application.create!(create_params)
+    flash[:notice] = "#{@application.application_name} was successfully created."
   end
   #alternative
   #def create
     #@application = Application.create!(params[:application])
-    #flash[:notice] = "#{@application.application_name} was successfully created."
+    #users/:id/applications/:application
   #end
+
+  private
+  def create_params
+    params.require(:application).permit(:application_name, :author,
+                                        :programming_language, :github_repository)
+  end
 
   #IMPORTANT: check asap if new and create implementation is correct! (must use nested resources, applications can be created only by a user)
   #TODO: (see pg.158) insert presence controls for current_user
